7357ccd3599c921093d0ba1b514739c74d970d0e
DUAwele <zthunderzthunder@gmail.com>
2025-03-01 12:22:54 -0600

    cleaning up files

--- searchJS/search.js
+++ searchJS/search.js
@@ -1,16 +1,13 @@
 async function printdata()
 {
 let apikey = "drhSKkesObsKIXdA8Y7HtC7mWaJxKA0d";
-
 let input = document.getElementById("searchbar").value;
-
 let info = null;
 
 let url = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${input}&limit=3`);
-
 info = await url.json();
-
 console.log(info)
+
 let picshere = document.getElementById("picshere")
 for(let i = 0; i < info.data.length; i++)
 {
@@ -18,4 +15,5 @@ let img = `<img src="${info.data[i].images.original.url}">` + info.data[i].title
 picshere.innerHTML += img;
 console.log(info.data[i].images.original.url)
 }
+
 }
